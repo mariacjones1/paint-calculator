98a6909aa83ce632373b0f56123de86e19f4700d
Maria <mariacjones1@hotmail.co.uk>
2023-03-16 16:11:40 +0000

    Add if statements and switch case to set price brackets and colour prices

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,49 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double costPerSquareMetre = 1.5;
+        int costBracket = 0;
+        double cost = 0;
+        double costPerSquareMetre = 0;
         Scanner wallLength = new Scanner(System.in);
         Scanner wallHeight = new Scanner(System.in);
+        Scanner paintColour = new Scanner(System.in);
         System.out.println("Enter the total wall length:");
         double inputLength = wallLength.nextDouble();
         System.out.println("Enter the total wall height:");
         double inputHeight = wallHeight.nextDouble();
         double wallArea = inputLength * inputHeight;
         System.out.println("Wall area: " + wallArea + "m2");
-        double totalCost = costPerSquareMetre * wallArea;
-        System.out.println("Total cost: £" + totalCost);
+        System.out.println("Enter the paint colour (blue, pink or yellow):");
+        String paint = paintColour.nextLine().toLowerCase();
+
+        if (paint.equals("blue")) {
+            costPerSquareMetre = 1.5;
+        } else if (paint.equals("pink")) {
+            costPerSquareMetre = 2;
+        } else if (paint.equals("yellow")) {
+            costPerSquareMetre = 2.5;
+        } else {
+            System.out.println("Invalid paint colour.");
+        }
+
+        if (wallArea < 50) {
+            costBracket = 1;
+        } else if (wallArea < 100) {
+            costBracket = 2;
+        }
+
+        switch(costBracket) {
+            case 1:
+                cost = 125;
+                break;
+            case 2:
+                cost = 250;
+                break;
+            default:
+                cost = costPerSquareMetre * wallArea;
+                break;
+        }
+
+        System.out.println("Total cost: £" + cost);
     }
 }
