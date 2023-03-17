9546dff48b85847e4ad4403af68b1395a2789bb7
Maria <mariacjones1@hotmail.co.uk>
2023-03-17 09:19:55 +0000

    Add do-while loop to paint colour selection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double cost = 0;
+        double cost;
         double costPerSquareMetre = 0;
         Scanner wallLength = new Scanner(System.in);
         Scanner wallHeight = new Scanner(System.in);
@@ -16,15 +16,19 @@ public class Main {
         double inputHeight = wallHeight.nextDouble();
         double wallArea = inputLength * inputHeight;
         System.out.println("Wall area: " + wallArea + "m2");
-        System.out.println("Enter the paint colour (blue, pink or yellow):");
-        String paint = paintColour.nextLine().toLowerCase();
 
-        switch (paint) {
-            case "blue" -> costPerSquareMetre = 1.5;
-            case "pink" -> costPerSquareMetre = 2;
-            case "yellow" -> costPerSquareMetre = 2.5;
-            default -> System.out.println("Invalid paint colour.");
-        }
+        do {
+            System.out.println("Enter the paint colour (blue, pink or yellow):");
+            String paint = paintColour.nextLine().toLowerCase();
+
+            switch (paint) {
+                case "blue" -> costPerSquareMetre = 1.5;
+                case "pink" -> costPerSquareMetre = 2;
+                case "yellow" -> costPerSquareMetre = 2.5;
+                default -> System.out.println("Invalid paint colour, please try again.");
+            }
+        } while (costPerSquareMetre == 0);
+
 
         int costBracket = wallArea < 50 ? 1 : 2;
 
